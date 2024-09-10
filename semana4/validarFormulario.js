6b8bc485ba2b992891f769722506bc607635f19e
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-09-10 19:33:26 -0300

    Adicionado o arquivo validarFormulario para validação dos campos do formulário com JavaScript

---
+++ semana4/validarFormulario.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('submit').addEventListener('click', () => {
+        if(validarFormulario()){
+            console.log("Tudo OK");
+        }
+        else{
+            console.log("Erro");
+        }
+    })
+   
+    document.getElementById('nome').addEventListener('blur', validarNome);
+});
+
+function validarNome() {
+    const nome = document.getElementById('nome').value;
+    // Expressão regular para validar que o nome contém apenas letras e espaços
+    const invalido = /^[A-Za-z\s]+$/;
+    const errorElement = document.getElementById('error-name');
+    
+    if (!invalido.test(nome) || nome.trim() === '') {
+        errorElement.textContent = "Nome Inválido";
+        errorElement.style.color = 'red'; // Define a cor do texto para vermelho
+        return false;
+    } else {
+        errorElement.textContent = ""; //remove a mensagem de erro após o nome ser válido
+        return true;
+    }
+}
+
+
+function validarFormulario() {
+    const validacoes = [
+        validarNome(),
+    ];
+
+    return validacoes.every(Boolean); // Somente submeter se todos forem válidos
+}
